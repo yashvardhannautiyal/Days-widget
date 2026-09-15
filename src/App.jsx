7fa05f419832794e2813f087db361b046da2ec17
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-09-15 19:42:30 +0530

    Added toggle theme

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,20 @@ function App() {
   const progressPercent = (daysPassed / totalDays) * 100;
 
   return (
-    <div className="border border-gray-700 flex flex-col w-xs p-6 rounded-3xl bg-gray-900">
+    <div
+      className="border border-gray-700 flex flex-col w-xs p-6 rounded-3xl bg-gradient-to-br from-white via-slate-100 to-indigo-100 dark:from-gray-950
+    dark:via-gray-900
+    dark:to-slate-700"
+    >
       {/* year + theme container  */}
       <div className="flex items-center justify-between w-full">
         <div>
-          <p className="text-gray-400 font-medium text-xs">YEAR</p>
-          <h1 className="text-white text-4xl font-semibold">{currentYear}</h1>
+          <p className="text-gray-600 dark:text-gray-400 font-semibold text-xs">
+            YEAR
+          </p>
+          <h1 className="text-gray-900 dark:text-white text-3xl font-semibold mt-1">
+            {currentYear}
+          </h1>
         </div>
 
         <div>
@@ -45,26 +53,34 @@ function App() {
       {/* days left + passed container  */}
       <div className="flex items-center gap-18 w-full mt-5">
         <div>
-          <p className="text-gray-400 font-medium text-xs">DAYS PASSED</p>
-          <p className="text-gray-50 text-4xl font-semibold">{daysPassed}</p>
+          <p className="text-gray-600 dark:text-slate-400 font-semibold text-xs">
+            DAYS PASSED
+          </p>
+          <p className="text-gray-900 dark:text-gray-50 text-4xl font-semibold mt-1">
+            {daysPassed}
+          </p>
         </div>
 
         <div>
-          <p className="text-gray-400 font-medium text-xs">DAYS LEFT</p>
-          <p className="text-gray-50 text-4xl font-semibold">{daysRemaining}</p>
+          <p className="text-gray-600 dark:text-slate-400 font-semibold text-xs">
+            DAYS LEFT
+          </p>
+          <p className="text-gray-900 dark:text-gray-50 text-4xl font-semibold mt-1">
+            {daysRemaining}
+          </p>
         </div>
       </div>
 
       {/* percentage  */}
       <div className=" mt-5">
         <div>
-          <p className="text-gray-400 font-medium text-sm">
+          <p className="text-gray-600 dark:text-slate-400 font-semibold text-xs">
             {progressPercent.toFixed(1)}% completed{" "}
           </p>
         </div>
-        <div className="w-full h-2 bg-blue-950 rounded-full overflow-hidden mt-2">
+        <div className="w-full h-2 bg-gray-200 dark:bg-blue-950 rounded-full overflow-hidden mt-2">
           <div
-            className="h-full bg-white rounded-full"
+            className="h-full bg-gray-900 dark:bg-white rounded-full"
             style={{ width: `${progressPercent}%` }}
           ></div>
         </div>
